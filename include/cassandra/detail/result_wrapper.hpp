81ac0e6c19c4bd07d4d2116dcb7e539f8bb82276
kostuwan <konsor-79@mail.ru>
2026-03-29 02:07:36 +0300

    wip

---
+++ include/cassandra/detail/result_wrapper.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+
+#include <cassandra/io/protocol/types.hpp>
+#include <cassandra/cassandra_fwd.hpp>
+
+namespace cassandra::detail {
+class ResultWrapper {
+public:
+    ResultWrapper(
+        std::vector<io::protocol::BytesBuffer>&& rows_content,
+        io::Int columns_count,
+        io::Int rows_count
+    )
+        : _rows_content(std::move(rows_content)),
+          _columns_count(columns_count),
+          _rows_count(rows_count) {}
+
+    bool Empty() const { return _rows_content.empty(); }
+
+    auto RowsAffected() const { return _rows_count; }
+
+    auto ColumnsAffected() const { return _columns_count; }
+
+    std::span<const io::protocol::BytesBuffer> RowsContentView() const {
+        return _rows_content;
+    }
+
+private:
+    std::vector<io::protocol::BytesBuffer> _rows_content;
+
+    io::Int _columns_count;
+    io::Int _rows_count;
+};
+}  // namespace detail
--- src/io/protocol/response_message.hpp
+++ src/io/protocol/response_message.hpp
@@ -6,9 +6,10 @@
 #include <cassandra/io/protocol/lz4_utils.hpp>
 #include <cassandra/io/protocol/message.hpp>
 #include <cassandra/io/protocol/types.hpp>
-#include <cassandra/result_set.hpp>
 #include <io/protocol/column_option.hpp>
 #include <io/protocol/events/schema_change.hpp>
+#include <memory>
+#include <cassandra/result_set.hpp>
 #include <userver/logging/log.hpp>
 #include <utility>
 #include <variant>
@@ -235,16 +236,16 @@ public:
     ResultSet GetResultSet() {
         if (_kind != ResultKind::kRows) {
             LOG_DEBUG("return empty result set");
-            return {};
+            return ResultSet{nullptr};
         }
         auto& row_kind = std::get<RowKind>(_payload);
         LOG_DEBUG("GET ROW KIND");
         LOG_DEBUG("RETURN RESULT SET");
-        return ResultSet{
+        return ResultSet{std::make_shared<cassandra::detail::ResultWrapper>(
             std::move(row_kind.rows_content),
             row_kind.columns_count,
             row_kind.rows_count
-        };
+        )};
     }
 
     io::ShortBytes GetPreparedStatementId() {
